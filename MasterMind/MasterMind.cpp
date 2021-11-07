8c42bdf46546fbbd215c14a62b0cce26cd0dda6e
Belkis Vasquez <bvasquez@itla.edu.do>
2021-11-06 20:08:40 -0400

    Estructura

--- MasterMind/MasterMind.cpp
+++ MasterMind/MasterMind.cpp
@@ -1,5 +1,12 @@
-// MasterMind.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// MasterMind 
+//Belkis Vasquez 1085273
+//Victor Toribio
+//Oscar Durán
+//Edgar Vasquez
+//Juan Avila
+//Paul 
+//06/11/2021 (Sabado 6 de noviembre del 2021)
+
 
 #include <iostream>
 
@@ -7,13 +14,3 @@ int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
